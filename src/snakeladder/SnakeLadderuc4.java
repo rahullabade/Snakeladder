b2ac39b40542f60dd39c45cf4059e98e59d9d6f0
RAHUL LABADE <rahullabade05@gmail.com>
2021-09-28 16:34:39 +0530

    Added: Content of the Code

---
+++ src/snakeladder/SnakeLadderuc4.java
@@ -0,0 +1,42 @@
+package snakeladder;
+public class SnakeLadderuc4 {
+	
+	public static void main(String[] args) {
+
+		System.out.println("Starting of Snake and Ladder Game");
+			int ladder = 1;
+			int snake = 2 ;
+			int urPresentStep = 0;
+			System.out.println("Starting the Game and You are on step" +urPresentStep);
+			while(urPresentStep < 100)
+			{
+
+				int dice= (int) Math.floor(Math.random()* 6 + 1);
+				System.out.println("The number on dice is " + dice);
+				int option = (int) Math.floor(Math.random() * 3);
+				System.out.println("If you get 0 it is noPlay, it is ladder.2 it is snake ");
+				System.out.println("You got option"+option);
+				if(option == ladder)
+				{
+					urPresentStep += dice;
+					System.out.println("You are on Step" + urPresentStep);
+					
+				}
+
+					else if(option == snake)
+
+					{
+
+							urPresentStep -= dice;
+							if(urPresentStep < 0)
+								urPresentStep = 0;
+							System.out.println("You are on step"  + urPresentStep);
+					}
+					else
+					{
+							System.out.println("You are on step"  + urPresentStep);
+					}
+
+}
+	}
+}
